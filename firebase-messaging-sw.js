c5ae514fdd1353ce1ddab44310f1659f098ab9e5
dfx81 <danialfitrighazali@gmail.com>
2025-10-24 12:38:22 +0800

    add indexeddb

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js");
+importScripts("localforage.min.js");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,39 +19,41 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
+messaging.onBackgroundMessage(async (payload) => {
     console.log(
         '[firebase-messaging-sw.js] Received background message ',
         payload
     );
 
+    localforage.setItem("test", "OK");
+
     // Send PayLoad to open tabs to store data
     clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
         for (const client of clientList) {
-        client.postMessage(payload.data);
+            client.postMessage(payload.data);
         }
     });
 
     // Customize notification here
     const notificationTitle = payload.data["title"];
     let notificationIcon = '';
-                if(payload.data["type"].toLowerCase() == "up"){
-                    notificationIcon = 'web/good.png' ;
-                }
-                else{
-                    notificationIcon = 'web/failure.png';
-                }
-                
-                const notificationOptions = {
-                    body: payload.data["body"],
-                    icon: notificationIcon,
-                    data: {
-                        url: "/history"
-                    }
-                };
+    if (payload.data["type"].toLowerCase() == "up") {
+        notificationIcon = 'web/good.png';
+    }
+    else {
+        notificationIcon = 'web/failure.png';
+    }
+
+    const notificationOptions = {
+        body: payload.data["body"],
+        icon: notificationIcon,
+        data: {
+            url: "/history"
+        }
+    };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
-                console.log(payload.data)
+    console.log(payload.data)
 });
 
 self.addEventListener('notificationclick', (event) => {
